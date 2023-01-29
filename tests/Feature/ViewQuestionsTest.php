e243516234e80664c907870c73ec1551fd0448b6
wlight <wormside@gmail.com>
2023-01-29 13:29:32 +0800

    test view questions

---
+++ tests/Feature/ViewQuestionsTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_user_can_view_questions()
+    {
+        // 1.假设 /questions 路由存在
+        // 2.访问链接 /questions
+        $test = $this->get('/questions');
+        // 3.正常返回 200
+        $test->assertStatus(200);
+    }
+}
